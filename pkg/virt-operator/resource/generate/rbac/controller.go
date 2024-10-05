7753f027748684ce64043fc980439072d691bda2
Alay Patel <alayp@nvidia.com>
2024-10-05 00:24:51 -0400

    add vmi dra status controller
    Signed-off-by: Alay Patel <alayp@nvidia.com>

--- pkg/virt-operator/resource/generate/rbac/controller.go
+++ pkg/virt-operator/resource/generate/rbac/controller.go
@@ -529,6 +529,20 @@ func newControllerClusterRole() *rbacv1.ClusterRole {
 					"watch",
 				},
 			},
+			{
+				APIGroups: []string{
+					"resource.k8s.io",
+				},
+				Resources: []string{
+					"resourceslices",
+					"resourceclaims",
+				},
+				Verbs: []string{
+					"list",
+					"watch",
+					"get",
+				},
+			},
 		},
 	}
 }
